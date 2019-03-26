2564fa0d409688f8f6d00f2d8a23e2d0f821558a
Xudong An <axd8911@hotmail.com>
2019-03-25 22:51:09 -0500

    Create 1_1_Is_Unique.py

---
+++ 1_1_Is_Unique.py
@@ -0,0 +1,19 @@
+def isUnique(strs):
+
+	n = 1
+	for item in strs:
+		if item not in strs[:n-1]:
+			n = n + 1
+		else:
+			return False
+			
+	return True
+
+
+def main():
+	strs = 'qwertyuiop[['
+	output = isUnique(strs)
+	print (output)
+
+
+main()
