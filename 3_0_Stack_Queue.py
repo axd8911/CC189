073ed1731aee242f81158428217b8652a21ea320
Xudong An <axd8911@hotmail.com>
2019-04-08 17:59:56 -0500

    Update 3_0_Stack_Queue.py

--- 3_0_Stack_Queue.py
+++ 3_0_Stack_Queue.py
@@ -2,24 +2,35 @@ class Stack:
 	def __init__(self):
 		self.items = []
 		
-	def isEmpty():
+	def isEmpty(self):
 		return self.items == []
 		
-	def pop():
+	def pop(self):
 		return self.items.pop()
 		
-	def push(item):
+	def push(self,item):
 		return self.items.append(item)
 		
-	def peek():
+	def peek(self):
 		return self.items[-1]
 		
 class Queue:
 	def __init__(self):
 		self.items = []
 		
-	def enqueue(item):
+	def enqueue(self,item):
 		return self.items.insert(0,item)
 		
-	def dequeue(item):
+	def dequeue(self):
 		return self.items.pop()
+		
+
+a = Stack()
+a.push(1)
+a.push(2)
+
+a.pop()
+a.pop()
+a.pop()
+
+print (a.items)
